9cb0cd59a900bbfeed7f70cd098614e51bf0e6fb
Michal Cichra <michal@o2h.cz>
2015-05-22 14:36:42 +0200

    generated gem by bundler

---
+++ lib/rack/x_served_by.rb
@@ -0,0 +1,7 @@
+require "rack/x_served_by/version"
+
+module Rack
+  module XServedBy
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/x_served_by/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module XServedBy
+    VERSION = "0.1.0"
+  end
+end
