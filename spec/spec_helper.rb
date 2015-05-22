f2c19072a3f8cdc3b24031a99fdbba09ca31801b
Michal Cichra <michal@o2h.cz>
2015-05-22 15:27:08 +0200

    integrate code climate test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@
 # it.
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
+
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
